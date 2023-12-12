22e475b4fd29d836fa4591b1b6bea0be859e5b6c
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-12 13:21:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    for (let i = 0; i < str.length; i++) {
+        let isRepeated = false;
+        for (let j = 0; j < str.length; j++) {
+            if (i !== j && str[i] === str[j]) {
+                isRepeated = true;
+                break;
+            }
+        }
+        if (!isRepeated) {
+            return str[i];
+        }
+    }
+    return null;
 }
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
+
+ 
